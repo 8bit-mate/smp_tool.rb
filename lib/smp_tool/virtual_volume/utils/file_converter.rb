80ce3e2eb1b13d98f414f58456ed990a110d75c7
8bit-m8 <you@example.com>
2024-03-05 20:45:21 +0300

    refactor the FileConverter

--- lib/smp_tool/virtual_volume/utils/file_converter.rb
+++ lib/smp_tool/virtual_volume/utils/file_converter.rb
@@ -10,11 +10,11 @@ module SMPTool
         class << self
           private
 
-          def calc_n_clusters(str)
+          def _calc_n_clusters(str)
             (str.length.to_f / CLUSTER_SIZE).ceil
           end
 
-          def make_header(ascii_filename, n_clusters, extra_word)
+          def _make_header(ascii_filename, n_clusters, extra_word)
             {
               status: PERM_ENTRY,
               filename: Filename.new(ascii: ascii_filename).radix50,
@@ -25,23 +25,26 @@ module SMPTool
             }
           end
 
-          def make_data(data, &block)
-            str = data.map(&block)
-                      .join("\r\n")
-                      .prepend(0x0A.chr)
-                      .prepend(0x0D.chr)
-                      .concat(0x0D.chr)
-                      .concat(0x0A.chr)
-                      .concat(0x00.chr)
+          def _make_data(data, &block)
+            str = _process_lines(data, &block)
+            str.ljust(_calc_n_clusters(str) * CLUSTER_SIZE, PAD_CHR)
+          end
 
-            str.ljust(calc_n_clusters(str) * CLUSTER_SIZE, PAD_CHR)
+          def _process_lines(data, &block)
+            data.map(&block)
+                .join("\r\n")
+                .prepend(0x0A.chr)
+                .prepend(0x0D.chr)
+                .concat(0x0D.chr)
+                .concat(0x0A.chr)
+                .concat(0x00.chr)
           end
         end
 
         def self.hash_to_data_entry(f_hash, extra_word, &block)
-          data = make_data(f_hash[:data], &block)
+          data = _make_data(f_hash[:data], &block)
 
-          header_params = make_header(f_hash[:filename], calc_n_clusters(data), extra_word)
+          header_params = _make_header(f_hash[:filename], _calc_n_clusters(data), extra_word)
 
           DataEntry.new(
             header: DataEntryHeader.new(header_params),
